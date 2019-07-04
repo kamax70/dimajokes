df74048bac70a61da325ed5ee9500ac9b32a3536
mak.kalashnikov <mak.kalashnikov@gmail.com>
2019-07-04 22:03:41 +0300

    init

---
+++ src/main/java/ru/dimajokes/DimajokesApplication.java
@@ -0,0 +1,13 @@
+package ru.dimajokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimajokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimajokesApplication.class, args);
+	}
+
+}
