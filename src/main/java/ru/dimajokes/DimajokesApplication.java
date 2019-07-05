b3c0e8d63875373a41117f3f11d60a0a955839f4
mkalashnikov <mak.kalashnikov@gmail.com>
2019-07-05 10:03:31 +0300

    stop

--- src/main/java/ru/dimajokes/DimajokesApplication.java
+++ src/main/java/ru/dimajokes/DimajokesApplication.java
@@ -1,15 +1,13 @@
 package ru.dimajokes;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+//@SpringBootApplication
 @EnableScheduling
 public class DimajokesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DimajokesApplication.class, args);
+//		SpringApplication.run(DimajokesApplication.class, args);
 	}
 
 }
