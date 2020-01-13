85e89dd3ac8ea39f9316483cc84e62e34364dab6
Vitaly Sirotkin <vsirotkin@vtb.ru>
2020-01-13 18:09:49 +0300

    add egor

---
+++ src/main/java/ru/dimajokes/BotConfig.java
@@ -0,0 +1,22 @@
+package ru.dimajokes;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "telegram")
+public class BotConfig {
+
+    private String botToken;
+    private Map<Long, ConfigEntry> jokers;
+
+    @Data
+    public static class ConfigEntry {
+        List<String> names;
+    }
+}
